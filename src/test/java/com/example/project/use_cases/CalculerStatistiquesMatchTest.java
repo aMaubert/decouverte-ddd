95c86cdc1686c610f24cef09aaffd24b9704b15c
Allan Maubert <amaubert@medissimo.fr>
2021-05-07 16:37:29 +0200

    comment all statistique match tests

--- src/test/java/com/example/project/use_cases/CalculerStatistiquesMatchTest.java
+++ src/test/java/com/example/project/use_cases/CalculerStatistiquesMatchTest.java
@@ -14,35 +14,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@RunWith(MockitoJUnitRunner.class)
-public class CalculerStatistiquesMatchTest extends TestCase {
-
-	private CalculerStatistiquesMatch calculerStatistiquesMatch;
-	private MockFaitDeJeuRepository mockFaitDeJeuRepository;
-	private MockMatchRepository mockMatchRepository;
-
-	@Before
-	public void init(){
-		this.mockFaitDeJeuRepository = new MockFaitDeJeuRepository();
-		this.mockMatchRepository = new MockMatchRepository();
-	}
-
-	/*@Test
-	public void testCalcul() {
-		calculerStatistiquesMatch = new CalculerStatistiquesMatch(mockFaitDeJeuRepository);
-		List<Match> matchs = Arrays.asList(new Match(1), new Match(2));
-
-		var statistiquesMatch = calculerStatistiquesMatch.calcul(matchs);
-		Assert.assertTrue(!statistiquesMatch.isEmpty());
-	}*/
-
-	/*@Test
-	public void devraitAvoir2FaitsDeJeuPour1Match() {
-		//Given:
-		List<FaitDeJeu> faitDeJeuListMatch = Arrays.asList(new FaitDeJeu(FaitDeJeuType.BUT_MARQUE), new FaitDeJeu(FaitDeJeuType.CARTON_JAUNE));
-		Equipe equipe = new Equipe(1);
-		Match match = new Match(1, faitDeJeuListMatch, equipe );
-		var matchs = Arrays.asList(new Match(1, faitDeJeuListMatch, equipe));
-	}
-*/
-}
+//@RunWith(MockitoJUnitRunner.class)
+//public class CalculerStatistiquesMatchTest extends TestCase {
+//
+//	private CalculerStatistiquesMatch calculerStatistiquesMatch;
+//	private MockFaitDeJeuRepository mockFaitDeJeuRepository;
+//	private MockMatchRepository mockMatchRepository;
+//
+//	@Before
+//	public void init(){
+//		this.mockFaitDeJeuRepository = new MockFaitDeJeuRepository();
+//		this.mockMatchRepository = new MockMatchRepository();
+//	}
+//
+//	/*@Test
+//	public void testCalcul() {
+//		calculerStatistiquesMatch = new CalculerStatistiquesMatch(mockFaitDeJeuRepository);
+//		List<Match> matchs = Arrays.asList(new Match(1), new Match(2));
+//
+//		var statistiquesMatch = calculerStatistiquesMatch.calcul(matchs);
+//		Assert.assertTrue(!statistiquesMatch.isEmpty());
+//	}*/
+//
+//	/*@Test
+//	public void devraitAvoir2FaitsDeJeuPour1Match() {
+//		//Given:
+//		List<FaitDeJeu> faitDeJeuListMatch = Arrays.asList(new FaitDeJeu(FaitDeJeuType.BUT_MARQUE), new FaitDeJeu(FaitDeJeuType.CARTON_JAUNE));
+//		Equipe equipe = new Equipe(1);
+//		Match match = new Match(1, faitDeJeuListMatch, equipe );
+//		var matchs = Arrays.asList(new Match(1, faitDeJeuListMatch, equipe));
+//	}
+//*/
+//}
